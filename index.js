b2a926587f401b00f9e83ea2ddcd6cf8622c3c26
lorenzo <lorenzomojica561@gmail.com>
2023-06-23 12:35:55 -0400

    adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Aki", "Bale", "Yuji"];
+const event = "graduation"
+
+function writeCards(names, event) {
+  let messages = []
+  for (let p = 0; p < names.length; p++) {
+    
+    messages.push (`Thank you, ${names[p]}, for the wonderful ${event} gift!`) 
+    
+  }
+  return messages
+}
+writeCards(names, event)
+
+function countDown(){
+let countDown = 10; 
+while (countDown > -1)
+  console.log(countDown--)
+}
